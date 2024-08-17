6ac87694d565751e965aeef06a4c7fc25fd87872
DeveloperRalsei <rizayildirim126@gmail.com>
2024-08-17 21:03:12 +0300

    #

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -20,16 +20,16 @@ const AboutMe: React.FC = () => {
   >
     I'm: <br />
     <List listStyleType='disc' >
-      <List.Item>
+      {/* <List.Item>
         <Anchor href='https://en.pronouns.page/@fluffyguy' target='_blank'>
           Transgender
           <Image src={"/images/trans.png"} display={"inline-block"} alt='trans' ms={3}  w={18} radius={2} />
         </Anchor>
-      </List.Item>
+      </List.Item> */}
       <List.Item>18 Years Old<Text size='xs' c={theme.colors.gray[6]} display={"inline"}> (11 Apr 2006)</Text></List.Item>
       <List.Item>
         <Tooltip label={<span>&#128557;</span>} withArrow position='right' >
-          <Text>Single</Text>
+          <Text>Alone</Text>
         </Tooltip>
       </List.Item>
       <List.Item>
@@ -38,7 +38,7 @@ const AboutMe: React.FC = () => {
         </Tooltip>
         <Image src={"/images/as-bayraklari-as.png"} alt='turkiye' display={"inline"} w={15} />
       </List.Item>
-      <List.Item>using <Anchor href="https://archlinux.org" target='_blank'>arch</Anchor> btw :P</List.Item>
+      <List.Item>using <Anchor href="https://archlinux.org" target='_blank'>arch</Anchor> btw :P <Image src={"/images/arch.png"} alt='arch' w={20} display={"inline-block"}/></List.Item> 
       <List.Item>and I like ice cream&#127848;<Text size='xs' display={"inline"} c={theme.colors.gray[6]}>(it would better if it was chocolate)</Text></List.Item>
       <List.Item>
         like to play:
