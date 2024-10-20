fe3837804a5a5813c36c10f1e82ed175cde86c21
DeveloperRalsei <rizayildirim126@gmail.com>
2024-10-20 18:08:21 +0300

    maybe i can buy a new internet tho. But I'm too poor for this 😭️.The phone internet is also considered dead (it's too slow anyway and it's of no use to me).

--- src/sections/ILEFT.tsx
+++ src/sections/ILEFT.tsx
@@ -1,5 +1,7 @@
 export default function ILEFT() {
-  return <div id="ILEFT">
-    I quit because my internet access was cut off.
-  </div>
+  return (
+    <div id="ILEFT" style={{ textAlign: "center" }}>
+      I won't be around for a while because my internet access has been cut off.
+    </div>
+  );
 }
