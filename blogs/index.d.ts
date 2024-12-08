fcc420160c81b9dd1e2b33e67d2415ac8502bd32
DeveloperRalsei <rizayildirim126@gmail.com>
2024-12-08 15:00:30 +0300

    Yayy I added blog system and it was so hard

--- blogs/index.d.ts
+++ blogs/index.d.ts
@@ -1,5 +1,3 @@
 import { Blog } from "@/types";
-import { blogInfo } from "./first.mdx";
-import { blogInfo as SecondBlog } from "./mrow.mdx";
 
-export default [blogInfo, SecondBlog] as Blog[];
+export default [] as Blog[];
--- src/sections/Blogs.tsx
+++ src/sections/Blogs.tsx
@@ -1,16 +1,44 @@
 import BlogCard from "@/components/BlogCard";
 import blogs from "@blogs";
-import { Stack, TextInput } from "@mantine/core";
+import { Stack, TextInput, Text } from "@mantine/core";
+import { useState } from "react";
 
 export default function Blogs() {
-  console.log(blogs);
+  const [search, setSearch] = useState("");
+  const filteredBlogs = !!search
+    ? blogs.filter((b) =>
+        [b.title, b.description, b.slug]
+          .filter((x) => x)
+          .join(" ")
+          .toLowerCase()
+          .includes(search.toLowerCase())
+      )
+    : blogs;
+
   return (
     <Stack>
-      <TextInput placeholder="Search..." />
+      <TextInput
+        placeholder="Search..."
+        value={search}
+        onChange={(e) => setSearch(e.target.value)}
+      />
 
-      {blogs.map((b, i) => {
+      {filteredBlogs.map((b, i) => {
         return <BlogCard blogInfo={b} key={i} />;
       })}
+
+      {!blogs.length && (
+        <Text w={"100%"} ta={"center"}>
+          No blogs yet {":("} <br />
+          Check back later
+        </Text>
+      )}
+
+      {/* {filteredBlogs.length === 0 && (
+        <Text w={"100%"} ta={"center"}>
+          There's nothing I can show you 3:
+        </Text>
+      )} */}
     </Stack>
   );
 }
