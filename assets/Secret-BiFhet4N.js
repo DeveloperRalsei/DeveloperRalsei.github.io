d5795f737578937fe5c9cf2e52c176d4c9840918
DeveloperRalsei <rizayildirim126@gmail.com>
2024-12-25 21:50:11 +0300

    Updates

---
+++ assets/Secret-BiFhet4N.js
@@ -0,0 +1,1 @@
+import{j as t,a1 as n}from"./index-CsAjSLrq.js";import{A as e}from"./Anchor-qC-fOsN3.js";const s=()=>t.jsxs(t.Fragment,{children:[t.jsx(n,{h:"300vh"}),t.jsx(e,{td:"underline",onClick:()=>open("https://jcw87.github.io/c2-sans-fight/","_blank"),children:"Don't click this link or you gonna have a bad time"})]});export{s as default};
