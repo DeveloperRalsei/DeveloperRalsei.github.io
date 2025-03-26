3ac487c683d21e5a7f50f9bfe54020979a710665
DeveloperRalsei <DeveloperRalsei@users.noreply.github.com>
2025-03-26 20:53:12 +0000

    deploy: 695dd84082dcbffc2899fa5ea098a80cb7f7bb45

---
+++ assets/Secret-Cq4DXLxs.js
@@ -0,0 +1,1 @@
+import{j as t,S as n}from"./index-C4xKe1kt.js";import{A as o}from"./Anchor-hvpNuLx9.js";import"./Text-DZ5Jmm8s.js";const s=()=>t.jsxs(t.Fragment,{children:[t.jsx(n,{h:"300vh"}),t.jsx(o,{td:"underline",onClick:()=>open("https://jcw87.github.io/c2-sans-fight/","_blank"),children:"Don't click this link or you gonna have a bad time"})]});export{s as default};
