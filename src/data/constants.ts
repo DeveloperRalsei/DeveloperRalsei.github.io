35d2055237b1318d5d5bbd47715b3160d3b4feeb
DeveloperRalsei <fluffycutegoatboi@gmail.com>
2025-04-08 07:34:19 +0300

    fixed event active constant

--- src/data/constants.ts
+++ src/data/constants.ts
@@ -4,4 +4,4 @@ export const BIRTHDAY_DATE = new Date("2025-04-11"),
     IS_TODAY_MYBIRTHDAY = new Date().getDay() == 11,
     MY_AGE = learnAge(new Date("2006-04-11")),
     IS_BIRTHDAY_EVENT_ACTIVE =
-        import.meta.env.DEV ?? new Date() < new Date("2025-04-12");
+        import.meta.env.DEV || new Date() < new Date("2025-04-12");
